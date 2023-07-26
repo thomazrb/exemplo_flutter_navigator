a047e987adc648f734024530b47dc7cec2dd982c
Thomaz Botelho <thomazrb@gmail.com>
2023-07-25 23:09:55 -0300

    Base inicial do código

---
+++ android/app/src/main/kotlin/com/example/exemplo_flutter_navigator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exemplo_flutter_navigator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/settings_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Settings Page'),
+      ),
+      body: const Center(
+        child: Text('Settings Page'),
+      ),
+    );
+  }
+}
